f74b490f625466f38dfeddf3d467f46be28d674a
Baste Nesse Buanes <baste@rain-games.com>
2019-09-18 10:43:09 +0200

    Updated the examples. They're a bit better, and have meta files so they work.

--- Samples~/Examples/Bar.cs
+++ Samples~/Examples/Bar.cs
@@ -4,6 +4,9 @@ namespace RuntimeScriptField.Example
 {
     public class Bar : MonoBehaviour
     {
-        
+        public void LogBar()
+        {
+            Debug.Log("A bar was added!");
+        }
     }
 }
--- Samples~/Examples/Foo.cs
+++ Samples~/Examples/Foo.cs
@@ -2,5 +2,10 @@
 
 namespace RuntimeScriptField.Example
 {
-    public class Foo : MonoBehaviour { }
+    public class Foo : MonoBehaviour {
+        public void LogFoo()
+        {
+            Debug.Log("A foo was added to the GameObject!");
+        }
+    }
 }
